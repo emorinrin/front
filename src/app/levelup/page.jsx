24b452a311e43734a0304fef954a73213e97ab95
emorinrin <yuya.ctd@gmail.com>
2024-12-11 00:20:49 +0900

    レベルアップ画面を調整

--- src/app/levelup/page.jsx
+++ src/app/levelup/page.jsx
@@ -2,6 +2,8 @@
 
 import { BackgroundDecoration } from "@/components/background-decoration";
 import Image from "next/image";
+import { Header } from "@/components/header";
+import { BottomNav } from "@/components/bottom-nav";
 
 export default function LevelUp() {
   const rank = 5; // 勇者のランクを指定（例: ランク5）
@@ -14,11 +16,13 @@ export default function LevelUp() {
   };
 
   return (
-    <div className="relative flex flex-col min-h-screen text-white font-rpg">
+    <div className="relative flex flex-col min-h-screen text-white font-rpg bg-[url('/35th2.jpg')] bg-cover">
+      <div className="absolute inset-0 bg-black opacity-50"></div>
       {/* 背景装飾 */}
       <BackgroundDecoration />
+      <Header />
 
-      <main className="flex flex-col items-center justify-center flex-1">
+      <main className="flex flex-col items-center justify-center flex-1 z-30">
         {/* 勇者の写真 */}
         <div className="relative w-32 h-32 mb-6 border-4 border-orange-400 shadow-lg">
           <Image
@@ -41,6 +45,7 @@ export default function LevelUp() {
           <p className="text-lg font-bold">勇者は レベル10に あがった！</p>
         </div>
       </main>
+      <BottomNav />
     </div>
   );
 }
